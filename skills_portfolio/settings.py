f6f2e989bca3e4d70e08b006c1ab293141baffdd
saumy <saumyasri1914@gmail.com>
2025-08-06 18:02:44 +0530

    env

--- skills_portfolio/settings.py
+++ skills_portfolio/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -20,18 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cyah$fbb53^r=ns8b2m6gr_qktb3$u0p3xcsdw6j$ddf-r)6yb'
-
+SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='vishapsripal0325@gmail.com'
-EMAIL_HOST_PASSWORD='rykfirffjrhtozvl'
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 # settings.py
 
@@ -113,7 +113,7 @@ CHANNEL_LAYERS={
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME':  config('DATABASE_NAME'),
     }
 }
 
